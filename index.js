7e0a740209c9c25360788545b9f90f52b415df16
jsephgd <jsephgd@gmail.com>
2022-04-15 14:15:55 -0400

    update

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
